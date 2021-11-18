f9bf5d4b40bb0e7ada5216ec09bda7c6fabf754b
Mark-Volshteyn <94350346+Mark-Volshteyn@users.noreply.github.com>
2021-11-18 11:38:23 +0300

    Add files via upload
    Split Inventory Validation to abstract layer and layer implementing Custom 6C plan type
    Unified naming

---
+++ Descriptor/InPlanConstantsPlan6C.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using PX.Data;
+using PX.Objects.IN.Overrides.INDocumentRelease;
+
+
+namespace PX.Objects.IN
+{
+	public class InPlanConstantsSOTransfers : INPlanConstants
+    {
+        public const string Plan6C = "6C";
+        public class plan6C : PX.Data.BQL.BqlString.Constant<plan6C>
+        {
+            public plan6C() : base(Plan6C) {; }
+        }
+
+        public static Type ToInclQtyFieldExtSOTransfers(string planType)
+        {
+            if (planType == InPlanConstantsSOTransfers.Plan6C)
+            {
+                return (typeof(INPlanTypeExtSOTransfers.usrInclQtySOTransfer));
+            }
+            else
+                return INPlanConstants.ToInclQtyField(planType);
+        }
+
+        public static string ToModuleFieldExtSOTransfers(string planType)
+        {
+            if (planType == InPlanConstantsSOTransfers.Plan6C)
+            {
+                return GL.BatchModule.SO;
+            }
+            else
+                return INPlanConstants.ToModuleField(planType);
+        }
+
+        public static void UpdateAllocatedQuantitiesWithCustomPlan(PXGraph graph, INItemPlan plan, bool revert = false)
+        {
+            string SoTransferPlanType = InPlanConstantsSOTransfers.Plan6C;
+            if (plan == null || !(plan.PlanType == SoTransferPlanType || plan.OrigPlanType == SoTransferPlanType))
+                return;
+
+            INPlanType plantype = INPlanType.PK.Find(graph, (plan.PlanType == SoTransferPlanType) ? plan.PlanType : plan.OrigPlanType);
+            //plantype = revert ? -plantype : plantype;
+            InventoryItem stkitem = InventoryItem.PK.Find(graph, plan.InventoryID);
+            bool updateOrigPlan = plan.OrigPlanType == SoTransferPlanType;
+            bool revertplan = updateOrigPlan ^ revert;
+
+            if (plan.InventoryID != null &&
+                plan.SubItemID != null &&
+                plan.SiteID != null &&
+                stkitem != null && stkitem.StkItem == true)
+            //only updating the sitestatus layer aince it is so plan w/o allocations
+            {
+                //BASE SOLUTION
+                //UpdateAllocatedQuantities<SiteStatus>(sender.Graph, plan, plantype, true);
+                //calls INPlanType targettype = GetTargetPlanType<TNode>(graph, plan, plantype);
+                //In SOOrderEntry screen, we assume OrigPlanType cannot be filled 
+
+                //SieStatus
+                SiteStatus palncurr = (SiteStatus)graph.Caches[typeof(SiteStatus)].Current;
+                SiteStatusExtSOTransfers targetext =
+                  (SiteStatusExtSOTransfers)graph.Caches[typeof(SiteStatus)].GetExtension<SiteStatusExtSOTransfers>(palncurr);
+
+                INPlanTypeExtSOTransfers plantypeext = plantype.GetExtension<INPlanTypeExtSOTransfers>();
+                targetext.UsrQtySOTransfer += (revertplan ? -plantypeext.UsrInclQtySOTransfer : plantypeext.UsrInclQtySOTransfer ?? 0) * plan.PlanQty;
+
+                palncurr.QtyAvail -= targetext.UsrInclQtySOTransfers == true ?
+                (revertplan ? -plantypeext.UsrInclQtySOTransfer : plantypeext.UsrInclQtySOTransfer ?? 0) * plan.PlanQty
+                : 0m;
+            }
+
+        }
+    }
+}
---
+++ Descriptor/SiteStatusAccumulatorPlan6CAttribute.cs
@@ -0,0 +1,52 @@
+﻿using PX.Data;
+
+namespace PX.Objects.IN.Overrides.INDocumentRelease
+{
+	public class SiteStatusAccumulatorSOTransfersAttribute : SiteStatusAccumulatorAttribute
+    {
+        //public SiteStatusAccumulatorSOTransfersAttribute{}
+
+        protected override bool PrepareInsert(PXCache sender, object row, PXAccumulatorCollection columns)
+        {
+            SiteStatus bal = (SiteStatus)row;
+            bool? origPersistEvenZero = bal.PersistEvenZero;
+            bal.PersistEvenZero = true;
+            if (!base.PrepareInsert(sender, row, columns))
+            {
+                bal.PersistEvenZero = origPersistEvenZero;
+                return false;
+            }
+            bal.PersistEvenZero = origPersistEvenZero;
+
+            //INSiteStatus bali = (INSiteStatus)bal;
+            SiteStatusExtSOTransfers balext = bal.GetExtension<SiteStatusExtSOTransfers>();
+            columns.Update<SiteStatusExtSOTransfers.usrQtySOTransfer>(balext.UsrQtySOTransfer, PXDataFieldAssign.AssignBehavior.Summarize);
+
+            if (sender.GetStatus(row) == PXEntryStatus.Inserted && IsZero(bal) && bal.PersistEvenZero != true)
+            {
+                if (sender.Locate(row) is SiteStatus located && ReferenceEquals(located, row))
+                {
+                    // only for Persist operation
+                    sender.SetStatus(row, PXEntryStatus.InsertedDeleted);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override bool IsZero(IStatus a)
+        {
+            if (!(a is SiteStatus))
+            {
+                return base.IsZero(a);
+            }
+            else
+            {
+                SiteStatusExtSOTransfers aext = ((SiteStatus)a).GetExtension<SiteStatusExtSOTransfers>();
+                return base.IsZero(a) && (aext.UsrQtySOTransfer == 0);
+            }
+        }
+    }
+
+}
